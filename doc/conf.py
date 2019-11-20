80fea775d91c6970d4dcc9b19a35ed2417e90ae4
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-11-20 21:13:22 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -79,13 +79,17 @@ autodoc_member_order = 'bysource'
 # a list of builtin themes.
 #
 # html_theme = 'guzzle'
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    # Disable showing the sidebar. Defaults to 'false'
+    'nosidebar': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
